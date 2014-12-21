e8c9081b2504830da7e9c96703015290b70fcf05
KS <bqd5e7n@yahoo.co.jp>
2014-12-21 19:04:50 +0900

    実機上でブート時のカーネルパラメータにacpi_sleep=nonvsを追加するスクリプトを作成

---
+++ script_dev/fixsleep.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -x
+pushd .
+
+# remount rw
+mount -o remount,rw /
+if [ 0 -ne $? ]; then
+  echo "[ERROR] Cannot remount / partition. Abort."
+  exit
+fi
+
+# create mount point
+cd /
+mkdir mnt_fixsleep
+
+# mount sda12
+devpath=`fdisk -l | grep "EFI System" | cut -d " " -f 1`
+mount ${devpath} /mnt_fixsleep
+sed -e "s/append/append acpi_sleep=nonvs/" /mnt_fixsleep/syslinux/root.A.cfg > /mnt_fixsleep/syslinux/root.A.cfg.new
+if [ 0 -eq $? ]; then
+  status=`grep "acpi_sleep=nonvs" /mnt_fixsleep/syslinux/root.A.cfg.new | wc -l`
+  if [ 2 -eq ${status} ]; then
+    mv /mnt_fixsleep/syslinux/root.A.cfg /mnt_fixsleep/syslinux/root.A.cfg.old
+    mv /mnt_fixsleep/syslinux/root.A.cfg.new /mnt_fixsleep/syslinux/root.A.cfg
+  fi
+fi
+
+# unmount sda12
+umount /mnt_fixsleep
+if [ 0 -eq $? ]; then
+  # delete mount point
+  cd /
+  rmdir mnt_fixsleep
+fi
+
+# remount read only
+mount -r -o remount /
+
+popd
