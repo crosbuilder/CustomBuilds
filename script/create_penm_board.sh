ce59704f8046069a5a661b713be7ba20713fb6aa
KS <bqd5e7n@yahoo.co.jp>
2015-03-22 00:21:33 +0900

    ・-mno-sse4.2を追加 ・repo startする処理を追加 ・setup_boardに--nousepkgオプションを追加

--- script/create_penm_board.sh
+++ script/create_penm_board.sh
@@ -9,21 +9,29 @@ cp -r overlay-x86-generic overlay-x86-pentiumm
 
 # コンパイルフラグからsse3を除去する
 cd overlay-x86-pentiumm
-sed -e 's/-msse3/-mno-sse3/g' -i make.conf
+sed -e 's/-msse3/-mno-sse3 -mno-ssse3 -mno-sse4.2/g' -e 's/^CHROMEOS_KERNEL_SPLITCONFIG="chromiumos-i386"$/CHROMEOS_KERNEL_SPLITCONFIG="chromiumos-pentiumm"/' -i make.conf
 
 # オーバレイ名を書き換える
 cd metadata
 sed -e 's/generic/pentiumm/g' -i layout.conf
 
+# ~/trunk/src/overlaysでrepo startする
+repo start my_overlay .
+
+# ~/trunk/src/third_party\chromiumos-overlayでrepo startする
+cd ~/trunk/src/third_party/chromiumos-overlay
+repo start my-chromiumos-overlay .
+
 # cros-board.eclassに新しいboardの名前を登録する
-cd ~/trunk/src/third_party/chromiumos-overlay/eclass
+cd eclass
 sed -e '/^\tx86-generic$/a \\tx86-pentiumm' -i cros-board.eclass
 
 read -p 'ready to setup board. continue?' status
 
+
 # boardのセットアップ
 cd ~/trunk/src/scripts
-./setup_board --board=x86-pentiumm
+./setup_board --board=x86-pentiumm --nousepkg
 
 # x86-genericでcros_workon startしているパッケージをx86-pentiummでもstartする
 cros_workon --board=x86-generic list | cut -d "/" -f 2 | xargs -L 1 cros_workon --board=x86-pentiumm start
