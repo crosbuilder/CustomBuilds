b346ed7f8158194d472471f4d1972b6d01f7e341
KS <bqd5e7n@yahoo.co.jp>
2015-03-21 23:40:16 +0900

    新規追加：p7zipパッケージを追加するスクリプト

---
+++ script/install_p7zip.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+source ./revisionup_ebuild.sh
+
+# BOARDが無ければ設定を要求する
+if [ -z "${BOARD}" ]; then
+	echo Please set BOARD. Abort.
+	exit 1
+fi
+
+# ~/trunk/src/third_party/portage_stableにブランチが無ければ作る
+cd ~/trunk/src/third_party/portage-stable
+b=`repo branches | grep portage-stable`
+if [ -z "$b" ]; then
+        echo create branches on portage-stable
+        repo start my-portage-stable . || exit 1
+	echo done.
+else
+        echo branch already exists. skip.
+fi
+
+# p7zipがない、またはバージョンが9.13ならパッケージをダウンロードする
+upgrade="false"
+if [ ! -e ./app-arch/p7zip ]; then
+	echo app-arch/p7zip package not found. download now.
+	upgrade="true"
+elif [ -e ./app-arch/p7zip/p7zip-9.13.ebuild ]; then
+	echo app-arch/p7zip package is old. download new package now.
+	upgrade="true"
+fi
+
+if [ ${upgrade} = "true" ]; then
+	cros_portage_upgrade --board=${BOARD} --upgrade app-arch/p7zip || exit 1
+else
+	echo app-arch/p7zip package found. skip.
+fi
+
+# 9.20.1.r4ではそのままではビルドできなかったのでebuildにパッチを当てる
+patch -p1 --dry-run < ~/myenv/patches/p7zip/p7zip-9.20.1-r4.ebuild.diff
+read -p 'above is results of dry-running patch. apply patch?[N/y]' status
+if [ ${status} = 'y' ]; then
+	patch -p1 < ~/myenv/patches/p7zip/p7zip-9.20.1-r4.ebuild.diff || exit 1
+	echo patch applied.
+fi
+
+
+# 依存関係を追加
+cd ~/trunk/src/third_party/chromiumos-overlay/virtual/target-chromium-os-dev
+search=`grep 'p7zip' target-chromium-os-dev-1.ebuild`
+if [ -z "${search}" ]; then
+        echo p7zip is not included in dev overlay. append to dev overlay now.
+        sed -e '/^RDEPEND="${RDEPEND}/a \\tapp-arch\/p7zip' -i target-chromium-os-dev-1.ebuild || exit 1
+        echo done
+        revisionup_ebuild
+else
+        echo p7zip is already included in dev overlay. skip.
+fi
+
