91b0899e8074a7024833360e81f7b36bebf75cb0
KS <bqd5e7n@yahoo.co.jp>
2015-04-23 23:21:09 +0900

    cros_workonする処理を追加

--- script/chrome_localize.sh
+++ script/chrome_localize.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# chromeをcros_workonする
+cros_workon --board=x86-pentiumm start chromeos-base/chromeos-chrome
+
 # chrome本体(chromeos-base/chromeos-chrome)のソースを/home/chromium/chrome_root配下にコピーする。
 # cros_workonしたchromeos-chromeは固定で~/chrome_root配下のソースをビルドする要になっている。
 chrome_root=/home/chromium/chrome_root
