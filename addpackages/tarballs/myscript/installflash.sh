eadba8be2540d1a468f1738614680a5f22239200
KS <bqd5e7n@yahoo.co.jp>
2015-04-23 23:24:32 +0900

    R41時点でのmyscriptパッケージ

---
+++ addpackages/tarballs/myscript/installflash.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+cleanup() {
+  cd /home/chronos/user/Downloads
+  rm -rf chrome_work
+# remount / read-only
+mount -r -o remount / 
+}
+
+if [ -e /opt/google/chrome/PepperFlash/manifest.json ]; then
+
+  local old_version=`grep version /opt/google/chrome/PepperFlash/manifest.json | grep -o -E '[0-9\.]*'`
+  echo Installed Flash Version: ${old_version}
+  echo Start update...
+else
+  echo "Flash isn't installed."
+  echo Start new installation...
+fi
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
+export PATH=${PATH}:/usr/local/bin
+# download chrome stable version(x86)
+echo Download Chrome package...
+echo 
+cd /home/chronos/user/Downloads
+mkdir chrome_work
+cd chrome_work
+
+if [ -e google-chrome-stable_current_i386.deb ]; then
+  rm google-chrome-stable_current_i386.deb
+fi
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb
+echo
+echo Download Completed.
+echo
+echo Extract package...
+ar x google-chrome-stable_current_i386.deb
+#tar xf data.tar.lzma --lzma
+xz -dv data.tar.xz
+tar xf data.tar
+echo 
+echo Extracted.
+echo
+echo get flash version...
+cd opt/google/chrome/PepperFlash
+flash_version=`grep version manifest.json | grep -o -E '[0-9\.]*'`
+if [ -n version ]; then
+  echo flash version is ${flash_version}
+else
+  echo failed to get flash version. Abort.
+  cleanup
+  exit 1
+fi
+
+echo copy plugins for rollback...
+local backupdir=/usr/local/myscript/flash_backup/${flash_version}
+if [ ! -e /usr/local/myscript/flash_backup ]; then
+  mkdir /usr/local/myscript/flash_backup
+fi
+mkdir ${backupdir}
+cp -r ../PepperFlash ${backupdir}
+cp ../libffmpegsumo.so ${backupdir}
+#cp ../libwidevinecdm.so ${backupdir}
+#cp ../libwidevinecdmadapter.so ${backupdir}
+
+echo install flash plugin
+
+# remount / writable
+mount -o remount,rw /
+
+echo copy plugins
+mkdir /opt/google/chrome/PepperFlash
+cp libpepflashplayer.so /opt/google/chrome/PepperFlash -f
+cp manifest.json /opt/google/chrome/PepperFlash -f
+cd ..
+cp libffmpegsumo.so /opt/google/chrome
+#cp libwidevinecdm.so /opt/google/chrome
+#cp libwidevinecdmadapter.so /opt/google/chrome
+
+echo add option to /etc/chrome_dev.conf
+
+sed -e '/^--ppapi-flash-path=\/opt\/google\/chrome\/PepperFlash\/libpepflashplayer.so/d' -i /etc/chrome_dev.conf 
+sed -e '/^--ppapi-flash-version.*/d' -i /etc/chrome_dev.conf
+
+echo '--ppapi-flash-path=/opt/google/chrome/PepperFlash/libpepflashplayer.so' >> /etc/chrome_dev.conf
+echo "--ppapi-flash-version=${flash_version}" >> /etc/chrome_dev.conf
+
+cleanup
+echo install Completed.
+
+echo 
+echo Please Restert your machine.
---
+++ addpackages/tarballs/myscript/touchpad/config-touchpad.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+CONFIG_FILE="/etc/X11/xorg.conf.d/40-touchpad-cmt.conf"
+
+# Check which type of touchpad is present.
+if grep -qi synaptics /proc/bus/input/devices; then
+  echo "Synaptics touchpad detected."
+  TOUCHPAD="synaptics"
+elif grep -qi alps /proc/bus/input/devices; then
+  echo "ALPS touchpad detected."
+  TOUCHPAD="alps"
+elif grep -qi elantech /proc/bus/input/devices; then
+  echo "Elantech touchpad detected."
+  TOUCHPAD="elantech"
+else
+  echo "No known touchpad found, exiting."
+  exit
+fi
+
+echo "Mounting the rootfs as read-write..."
+mount -o remount, rw /
+
+if [ ! -e "$CONFIG_FILE.bak" ]; then
+  echo "Creating backup for $CONFIG_FILE..."
+  cp $CONFIG_FILE $CONFIG_FILE.bak
+fi
+
+echo "Installing the configuration file for your touchpad..."
+rm $CONFIG_FILE
+cp ./$TOUCHPAD.xorg.conf $CONFIG_FILE
+
+echo "Configuration finished. Please reboot to make the change effective."
