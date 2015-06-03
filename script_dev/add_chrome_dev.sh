324f6b025b33ebdaaf09164119cc5dda4bea67b3
zui22904336 <zui22904336@gmail.com>
2015-06-03 22:24:04 +0900

    add_chrome_dev.shで引数にハイフンで始まる文字列を渡されるとエラーになる問題を修正

--- script_dev/add_chrome_dev.sh
+++ script_dev/add_chrome_dev.sh
@@ -5,12 +5,12 @@
 # どちらに出力する際も重複チェックして同じ行が含まれないようにする
 # $1 chrome_dev.confに出力する文字列。空白を含む場合はダブルクォートで囲む
 add_chrome_dev() {
-  chrome_dev=/tmp/chrome_dev.conf
+  chrome_dev=/etc/chrome_dev.conf
   if [ ! -e ${chrome_dev} ]; then
     echo chrome_dev.conf not found. Abort.
     exit 1
   fi
-  line=`grep "$1" ${chrome_dev}`
+  line=`grep -- "$1" ${chrome_dev}`
   if [ -z "${line}" ]; then
     mount -o remount,rw /
     if [ 0 -ne $? ]; then
@@ -25,7 +25,7 @@ add_chrome_dev() {
   history=${script_local}/chrome_dev_history.sh
   if [ -e ${history} ]; then
     # 既に同じ行が存在するか確認する
-    line=`grep "$1" ${history}`
+    line=`grep -- "$1" ${history}`
     if [ -z "${line}" ]; then
       echo "$1" >> ${history}
       echo add line comand to chrome_dev_history.
