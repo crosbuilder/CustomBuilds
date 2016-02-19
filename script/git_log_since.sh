b9d67ecfeefb933e2f80a978e7dce11259d2cc52
zui22904336 <zui22904336@gmail.com>
2016-02-19 20:39:39 +0900

    git logの末尾にピリオドが抜けていたので修正

--- script/git_log_since.sh
+++ script/git_log_since.sh
@@ -2,4 +2,4 @@
 # 以下のようにして、指定日付以降のコミットがあるかを一括検索する
 # find . -mexdepth 1 -exec ~/myenv/script/git_log_since.sh {} "yyyy-mm-dd HH:MM:SS" \; | less
 cd $1
-git log  --since="$2"
+git log  --since="$2" .
