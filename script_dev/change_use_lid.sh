252106457f63dc7d9963d940abc3458364a863b4
zui22904336 <zui22904336@gmail.com>
2016-02-19 20:40:47 +0900

    * /usr/share/power_manager/以下を書き換えるのをやめ、/var/lib/power_manager/以下を書き換えるように修正 * set_power_policyコマンドで変更内容をpowerdに送信する処理を追加

--- script_dev/change_use_lid.sh
+++ script_dev/change_use_lid.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-use_lid=/usr/share/power_manager/use_lid
-sudo mount -o remount,rw /
-status=`cat ${use_lid}`
-if [ 1 -eq ${status} ]; then
-  echo 'Current "use_lid_to_suspend" is enable.'
-  sudo sh -c "echo 0 > ${use_lid}"
-  echo '"use_lid_to_suspend" has been disabled'
+use_lid_base=/usr/share/power_manager/use_lid
+use_lid=/var/lib/power_manager/use_lid
+#sudo mount -o remount,rw /
+if [ ! -f ${use_lid} ]; then
+  status=`cat ${use_lid_base}`
 else
-  echo 'Current "use_lid_to_suspend" is disable.'
-  sudo sh -c "echo 1 > ${use_lid}"
-  echo '"use_lid_to_suspend" has been enabled.'
-
+  status=`cat ${use_lid}`
 fi
-sudo mount -o remount,ro /
-echo Please reboot your machine.
+if [ 1 -eq ${status} ]; then
+  echo 'Current "lid_closed_action" is "suspend".'
+  sudo sh -c "echo 0 > ${use_lid}"
+  sudo set_power_policy --lid_closed_action=do_nothing
+  echo '"lid_closed_action" has been changed to "do_nothing".'
+else
+  echo 'Current "lid_closed_action" is "do_nothing".'
+  sudo sh -c "echo 1 > ${use_lid}"
+  sudo set_power_policy --lid_closed_action=suspend
+  echo '"lid_closed_action" has been changed to "suspend".'
+fi
+echo 'Please reboot your machine.'
