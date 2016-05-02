86802dccd61e62f48f75415d4f383ab680cf9a41
zui22904336 <zui22904336@gmail.com>
2016-05-02 19:18:38 +0900

    chrome_mediaの有効・無効を切り替えるスクリプト

---
+++ script/disable_codec.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ebuildにパッチ当てをする(audio/mp3対策 )
+cd ~/trunk/src/third_party/chromiumos-overlay
+patch -p1 -R < ~/myenv/patches/chromeos-chrome/enable_codec.diff
+if [ $? -ne 0 ]; then
+  echo Failed to apply patch. Abort.
+  exit 1
+fi
+
