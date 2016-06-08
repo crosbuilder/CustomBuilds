838d509bcd1f9428affd08512eabc68708f1500e
zui22904336 <zui22904336@gmail.com>
2016-06-08 22:52:58 +0900

    mesa(vmwgfx_dri.so)関連のパッチ適用処理を追加

--- script/applypatches_chromiumos-overlay.sh
+++ script/applypatches_chromiumos-overlay.sh
@@ -15,6 +15,19 @@ if [ 0 -ne $? ]; then
 fi
 
 cd /home/chromium/trunk/src/third_party/chromiumos-overlay 
+if [ ! -d licenses/copyright-attribution/media-libs ]; then
+  mkdir licenses/copyright-attribution/media-libs
+  if [ 0 -ne $? ]; then
+    echo Failed to mkdir license directory. Abort.
+    exit 1
+  fi
+fi
+cp /home/chromium/myenv/patches/chromiumos-overlay/mesa/license licenses/copyright-attribution/media-libs/mesa
+if [ 0 -ne $? ]; then
+  echo Failed to copy licence file of mesa. Abort.
+  exit 1
+fi
+
 repo start my-chromiumos-overlay .
 cd chromeos-base/tty
 mv tty-0.0.1-r7.ebuild tty-0.0.1-r8.ebuild
@@ -23,3 +36,5 @@ if [ 0 -ne $? ]; then
   echo Failed to mv tty ebuild. Abort.
   exit 1
 fi
+
+
