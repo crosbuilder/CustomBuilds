3e5afbadc3e38995690e47baff8088fb8ea3ca3a
zui22904336 <zui22904336@gmail.com>
2016-06-18 06:41:09 +0900

    myscript パッケージ R51-8172.47

---
+++ addpackages/tarballs/myscript/enable_ipw2100.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+cd `dirname $0`
+source script_root.sh
+source addhistory.sh
+
+addhistory $0 "$@"
+
+# remount root rw
+mount -o remount,rw /
+
+# add modprobe to pre-strat.conf
+
+g=`grep "modprobe cfg80211" /etc/init/pre-startup.conf`
+if [ -z "$g" ]; then
+  echo pre-startup.conf is not modified.
+  cp /etc/init/pre-startup.conf /etc/init/pre-startup.conf.old
+  sed -e '/^ *static_node_tool/a modprobe cfg80211' -i /etc/init/pre-startup.conf
+  echo pre-startup.conf has been modified.
+else
+  echo pre-startup.conf is already modified. skip.
+fi
+
+# remove modprobe from shill.conf
+
+g=`grep '\#.*modprobe cfg80211.*$' /etc/init/shill.conf`
+if [ -z "$g" ]; then
+  echo shill.conf is not modified yet.
+  cp /etc/init/shill.conf /etc/init/shill.conf.old
+  sed -e 's/\(^ *modprobe cfg80211.*$\)/\#\1/' -e 's/\(^.*Failed to load cfg80211.*$\)/\#\1/' -i /etc/init/shill.conf
+  echo shill.conf has been modified.
+else
+  echo shill.conf is already modified. skip.
+fi
+
+# modify params of wpa_supplicant
+g=`grep Dwext /etc/init/wpasupplicant.conf`
+if [ -z "$g" ]; then
+  echo wpasupplicant.conf is not modified
+  cp /etc/init/wpasupplicant.conf /etc/init/wpasupplicant.conf.old
+  sed -e "s@/usr/sbin/wpa_supplicant\(.*$\)@/usr/sbin/wpa_supplicant -B -Dwext -i \${dev} \1 -c ${script_root}/wpa.conf@" -i /etc/init/wpasupplicant.conf
+  sed -e "/^respawn$/a respawn limit 20 20" -i /etc/init/wpasupplicant.conf
+  #sed -e "/exec minijail0/i sleep 3" -i /etc/init/wpasupplicant.conf
+  sed -e "/exec minijail0/i dev=\`${script_root}/find_wirelessdev.sh\`" -i /etc/init/wpasupplicant.conf
+  echo wpasupplicant.conf has been modified.
+else
+  echo wpasupplicant.conf is already modified skip.
+fi
+
+# create preload-network-drivers
+if [ ! -e /var/lib/preload-network-drivers ]; then
+  echo preload-network-drivers not found. create it.
+  echo ipw2100 > /var/lib/preload-network-drivers
+  echo preload-network-drivers created.
+else
+  g=`grep ipw2100 /var/lib/preload-network-drivers`
+  if [ -z "$g" ]; then
+    echo preload-network-drivers not contains ipw2100.
+    sed -e '1i ipw2100' -i /var/lib/preload-network-drivers
+    echo add entry ipw2100 to preload-network-drivers
+  else
+    preload-network-drivers found. skip.
+  fi
+fi
---
+++ addpackages/tarballs/myscript/init_vmware.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+cd `dirname $0`
+. ./script_root.sh
+. ./addhistory.sh
+
+addhistory $0 "$@"
+
+# NOTICE: cirrus uses this script too.
+
+# mount EFI-SYSLINUX partition
+root=`rootdev`
+#root=/dev/sda3
+syslinux=`echo ${root}|sed -e 's/\(3\|5\)/12/g'`
+echo mount ${syslinux} | tee /dev/tty1 | logger -t myscript
+
+mkdir /tmp/mnt
+mount ${syslinux} /tmp/mnt
+if [ 0 -ne $? ]; then
+	exit 1
+fi
+cd /tmp/mnt
+
+# rewrite kernel parameter
+cfgdirlist=(/boot /tmp/mnt)
+for cfgdir in ${cfgdirlist[@]}; do
+  cfglist=(root.A.cfg root.B.cfg usb.A.cfg)
+  cd ${cfgdir}
+  for cfg in ${cfglist[@]}; do
+    content=`cat syslinux/${cfg}`
+    content_new=`echo "${content}" | sed -e "s/i915.modeset=0/i915.modeset=1/g" -e "s/radeon.modeset=1/radeon.modeset=0/g" -e "s/nouveau.modeset=1//g"`
+    if [ "${content}" != "${content_new}" ]; then
+      echo modify ${cfg} | tee /dev/tty1 | logger -t myscript
+      echo "${content_new}" > syslinux/${cfg}
+    fi
+  done
+done
+
+#replace xorg mouse driver
+cd /etc/X11/xorg.conf.d
+mv 20-mouse.conf 20-mouse.conf.org
+cp /opt/myscript/20-vmmouse.conf /etc/X11/xorg.conf.d/
+echo Install vmmouse driver... | tee /dev/tty1 | logger -t myscript
+
+#umount EFI-SYSLINUX
+cd ${script_root}
+umount /tmp/mnt
+echo umount ${syslinux} | tee /dev/tty1 | logger -t myscript
+
+# disable vesa
+${script_root}/disable_vesa.sh nohistory
+
