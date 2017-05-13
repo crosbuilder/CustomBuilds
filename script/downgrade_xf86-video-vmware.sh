a02832517ca6f907e82db7003b58afdab2db62e5
zui22904336 <zui22904336@gmail.com>
2017-05-14 08:30:03 +0900

    xf86-video-vmwareを13.2.1->13.1.0にダウングレードする

---
+++ script/downgrade_xf86-video-vmware.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+myname=$0
+cd ${myname%/*}
+
+if [ -z ${BOARD} ]; then
+	echo "[ERROR] Please set BOARD".
+	exit 1
+fi
+
+# /tmp/portageに13.1.0のebuildを持ってくる
+
+cros_portage_upgrade --board=${BOARD} --upgrade x11-drivers/xf86-video-vmware-13.1.0
+if [ 0 -ne $? ]; then
+	echo "[ERROR]cros_portage_upgrade failed. Abort."
+	exit 1
+fi
+
+# 既存のebuildとManifestを消す
+
+cd ~/trunk/src/third_party/portage-stable/x11-drivers/xf86-video-vmware
+rm *.ebuild
+rm Manifest
+
+# 13.1.0のebuildをコピーする
+
+cp /tmp/portage/x11-drivers/xf86-video-vmware/xf86-video-vmware-13.1.0.ebuild .
+if [ 0 -ne $? ]; then
+	echo "[ERROR]copy ebuild to portage-stable failed. Abort."
+	exit 1
+fi
+
+# Manifestを再作成
+ebuild-x86-pentiumm xf86-video-vmware-13.1.0.ebuild manifest
+if [ 0 -ne $? ]; then
+	echo "[ERROR]Failed to create Manifest. Abort."
+	exit 1
+fi
+
+# テストコンパイル実行
+ebuild-x86-pentiumm xf86-video-vmware-13.1.0.ebuild compile
+if [ 0 -ne $? ]; then
+	echo "[ERROR]Failed to compile. Abort."
+	exit 1
+fi
+
