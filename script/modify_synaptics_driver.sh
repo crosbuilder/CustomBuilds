43c94b849a1d71dd584f9b4dcdc235e1c7085319
zui22904336 <zui22904336@gmail.com>
2017-08-26 17:38:21 +0900

    branchがなければ作る処理を追加

--- script/modify_synaptics_driver.sh
+++ script/modify_synaptics_driver.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# ~/trunk/src/third_party/portage-stableにブランチが無ければ作る
 cd ~/trunk/src/third_party/portage-stable
+b=`repo branches | grep portage-stable`
+if [ -z "$b" ]; then
+	echo create branches on portage-stable
+	repo start my-portage-stable .
+else
+	echo branch already exists. skip.
+fi
+
 patch -p1 < ~/myenv/patches/portage-stable/synaptic-compile-error.diff
 if [ $? -ne 0 ]; then
   echo apply patch failed.
