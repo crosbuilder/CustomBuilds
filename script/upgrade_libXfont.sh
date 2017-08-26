01bbdb827dd79bb78862b50812a2ef5166f572e5
zui22904336 <zui22904336@gmail.com>
2017-08-26 18:26:57 +0900

    upgrade_libXfont.shにR59の修正内容をマージ

--- script/upgrade_libXfont.sh
+++ script/upgrade_libXfont.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-# x86-xorg board で書き換えるパッケージをオーバレイディレクトリにコピーして修正を加える
+myname=$0
+cd ${myname%/*}
 
-pushd . > /dev/null
+source ./revisionup_ebuild.sh
 
-# libXfontをアップグレードする
-cd ~/trunk/src/third_party/portage-stable/
-repo start my-portage-stable .
+# ~/trunk/src/third_party/portage-stableにブランチが無ければ作る
+cd ~/trunk/src/third_party/portage-stable
+b=`repo branches | grep portage-stable`
+if [ -z "$b" ]; then
+	echo create branches on portage-stable
+	repo start my-portage-stable .
+else
+	echo branch already exists. skip.
+fi
+
+# libXfont 1.4.9 パッケージをダウンロードする。BOARDはx86-genericで固定
 echo upgrade libXfont to 1.5.2
-cros_portage_upgrade --board=x86-xorg --upgrade x11-libs/libXfont-1.5.2 || exit 1
+cros_portage_upgrade --board=x86-generic --upgrade x11-libs/libXfont-1.5.2 || exit 1
 echo done.
-
 
